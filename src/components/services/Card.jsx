6ce5a21202ce8b379c8d6674281045f0ddbeca7a
Dushani-Ranasinghe <uththaradushani@gmail.com>
2024-01-23 23:19:59 +0530

    Created services section

--- src/components/services/Card.jsx
+++ src/components/services/Card.jsx
@@ -1,16 +1,15 @@
 import React from "react";
-import "./Card.css"
-import Gift from "../../assets/giftbox/giftbox1.jpg"
+import "./Card.css";
 
-const Card = () => {
+const Card = ({ image, link, description,link_title }) => {
   return (
     <div className="card_container">
-      <div className="card_image">
-        <img src={Gift} alt=""/>
+      <div className="card_image_container">
+        <img src={image} alt="" className="card_img" />
       </div>
-      <div class="card__overlay">
-        <h2>Paris</h2>
-        <p>Eiffel Tower</p>
+      <div className="card_overlay">
+        <a href={link}>{link_title}</a>
+        <p>{description}</p>
       </div>
     </div>
   );
--- src/components/services/Services.jsx
+++ src/components/services/Services.jsx
@@ -1,20 +1,72 @@
-import React from 'react'
-import Card from './Card'
-import "./Services.css"
+import React from 'react';
+import Card from './Card';
+import "./Services.css";
+import Gift1 from "../../assets/giftbox/giftbox3.jpg"
+import Gift2 from "../../assets/giftcard/giftcard3.jpg"
+import Gift3 from "../../assets/giftcard/giftcard2.jpg"
+import Gift4 from "../../assets/giftcard/giftcard1.jpg"
+import Gift5 from "../../assets/giftcard/giftcard4.jpg"
+import Gift6 from "../../assets/giftjar/giftjar4.jpg"
+
+const cardData = [
+  {
+    image: Gift1, 
+    link: "#service1",
+    link_title:"Gift Boxes",
+    description: "Elegant gift box perfect for any special occasion."
+  },
+  {
+    image: Gift2, 
+    link: "#service1",
+    link_title:"Gift Cards",
+    description: "Artistic cards designed to make every occasion special."
+  },
+  {
+    image: Gift3, 
+    link: "#service1",
+    link_title:"Gift Jars",
+    description: "Adorable gift jar filled with sweet treats. "
+  },
+
+  {
+    image: Gift4, 
+    link: "#service1",
+    link_title:"Gift Baskets",
+    description: "Surprise your loved ones with our customized gift baskets."
+  },
+  {
+    image: Gift5, 
+    link: "#service1",
+    link_title:"Explosion boxes",
+    description: "Stand out collection of unique explosion boxes."
+  },
+  {
+    image: Gift6, 
+    link: "#service1",
+    link_title:"Event Deco",
+    description: "Transform events with stunning decorations."
+  },
+
+];
 
 const Services = () => {
   return (
+    <section className="services section" id="services">
     <div>
-    <div className='card_box'>
-        <Card/>
-        <Card/>
-        <Card/>
-        <Card/>
-        <Card/>
-        <Card/>
+      <div className='card_box'>
+        {cardData.map((card, index) => (
+          <Card
+            key={index}
+            image={card.image}
+            link={card.link}
+            link_title={card.link_title}
+            description={card.description}
+          />
+        ))}
+      </div>
     </div>
-    </div>
-  )
-}
+    </section>
+  );
+};
 
-export default Services
+export default Services;
