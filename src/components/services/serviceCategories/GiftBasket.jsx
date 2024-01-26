aaf983c89dc8d1d35ca9d5a79ffb8e70552088db
Dushani-Ranasinghe <uththaradushani@gmail.com>
2024-01-26 21:05:03 +0530

    Created Gift jar and Gift basket pages

--- src/components/services/serviceCategories/GiftBasket.jsx
+++ src/components/services/serviceCategories/GiftBasket.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import React from "react";
+import GiftBasketBackground from "../../../assets/giftbasket/gifbasket.jpg";
+import GiftBasket1 from "../../../assets/giftbasket/giftbasket1.jpg";
+import GiftBasket2 from "../../../assets/giftbasket/giftbasket2.jpg";
+import ProductLayoutModal from "../../modals/ProductLayoutModal";
 
 const GiftBasket = () => {
-  return (
-    <div>GiftBasket</div>
-  )
-}
 
-export default GiftBasket
+  const galleryImages = [
+    { id: 1, src: GiftBasket1, alt: "Gift Box 1", subImages: [GiftBasket1, GiftBasket2,GiftBasketBackground] },
+    { id: 2, src: GiftBasket2, alt: "Gift Box 2", subImages: [GiftBasket2, GiftBasketBackground, GiftBasket1] },
+  ];
+
+  return (
+    <div className="gift_container">
+     <ProductLayoutModal bannerImage={GiftBasketBackground} title="Gift Basket" description = "Explore our beautiful Gift Basket collection" galleryImages={galleryImages}/>
+    </div>
+  );
+};
+
+export default GiftBasket;
--- src/components/services/serviceCategories/GiftJar.jsx
+++ src/components/services/serviceCategories/GiftJar.jsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import React from "react";
+import GiftJarBackground from "../../../assets/giftjar/giftjar1.jpg";
+import GiftJar1 from "../../../assets/giftjar/giftjar2.jpg";
+import GiftJar2 from "../../../assets/giftjar/giftjar3.jpg";
+import GiftJar3 from "../../../assets/giftjar/giftjar4.jpg";
+import ProductLayoutModal from "../../modals/ProductLayoutModal";
 
 const GiftJar = () => {
-  return (
-    <div>GiftJar</div>
-  )
-}
 
-export default GiftJar
+  const galleryImages = [
+    { id: 1, src: GiftJar1, alt: "Gift Box 1", subImages: [GiftJar1, GiftJar2, GiftJar3,GiftJarBackground] },
+    { id: 2, src: GiftJar2, alt: "Gift Box 2", subImages: [GiftJar2, GiftJar3, GiftJar1] },
+    { id: 3, src: GiftJar3, alt: "Gift Box 3", subImages: [GiftJar3, GiftJarBackground, GiftJar1] },
+  ];
+
+  return (
+    <div className="gift_container">
+     <ProductLayoutModal bannerImage={GiftJarBackground} title="Gift Jars" description = "Explore our beautiful Gift Jar collection" galleryImages={galleryImages}/>
+    </div>
+  );
+};
+
+export default GiftJar;
